25ba5f300422a44cbdd08dfb90c8659d5d5fe944
asmaakbouchi <asmaakbouchi@gmail.com>
2024-10-01 00:10:57 +0200

    last commit

--- script.js
+++ script.js
@@ -96,12 +96,3 @@ form.addEventListener("submit",(e)=>{
     e.preventDefault();
     sendEmail();
 })
-
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-  }
